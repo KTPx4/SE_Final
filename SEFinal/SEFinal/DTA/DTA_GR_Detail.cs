b6edc832edef1a33f4f31abeaa3e135b9012c105
KTPx4 <px4.vnd@gmail.com>
2023-04-23 09:12:46 +0700

    create project winform

---
+++ SEFinal/SEFinal/DTA/DTA_GR_Detail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEFinal.DTA
+{
+    public class DTA_GR_Detail
+    {
+    }
+}
---
+++ SEFinal/SEFinal/frmMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEFinal
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Style();
+        }
+
+        public void Style() 
+        {
+            foreach (Control ctl in this.Controls)
+            {
+                try
+                {
+                    System.Windows.Forms.Control Mdi = (MdiClient)ctl;
+
+                    Mdi.BackColor = System.Drawing.Color.White;
+
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
+    }
+}
